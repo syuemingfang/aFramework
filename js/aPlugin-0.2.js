07b15731b3c313729cbedb95b3f1d554c55e5f8b
syuemingfang <syuemingfang@yahoo.com.tw>
2013-03-17 17:56:03 +0800

    Start Version 0.2

---
+++ js/aPlugin-0.2.js
@@ -0,0 +1,7 @@
+document.write("<script src='js/aFontResize-0.2.js'><\/script>");
+document.write("<script src='js/aBanner-0.2.js'><\/script>");
+document.write("<script src='js/aMarquee-0.2.js'><\/script>");
+document.write("<script src='js/aLightbox-0.2.js'><\/script>");
+document.write("<script src='js/aShare-0.2.js'><\/script>");
+document.write("<script src='js/aChange-0.2.js'><\/script>");
+document.write("<script src='js/aView-0.2.js'><\/script>");
---
+++ js/aReplace-0.2.js
@@ -0,0 +1,31 @@
+﻿;(function($){	   
+	$.fn.aReplace=function(settings){
+		var _defaultSettings={
+			bind:'click',
+			animate:'fade',
+			originalText:'',
+			replaceText:'',
+			mode:'string',
+			callback:function(){
+			}
+		};
+		var _settings=$.extend(_defaultSettings,settings);
+		return this.each(function(){aReplace($(this),_settings);});
+		function aReplace($this,_settings){
+			$this.on(_settings.bind ,function() {
+												
+											switch(_settings.mode){
+				case 'string':
+					var pattern = new RegExp(_settings.originalText,"g");
+					var str = $("body").html();
+					var result = str.replace(pattern,_settings.replaceText);
+					$("body").html(result);
+				break;	
+				}
+			return false;				
+			});
+		}
+		
+		
+	};
+})(jQuery);
